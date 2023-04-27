97db1ddee1cba3d2a2743636e3af62cb86f62387
Piyush <palpiyush2003@gmail.com>
2023-04-27 11:38:57 +0530

    LinkedList

---
+++ src/com/Piyush/C56_LinkedList.java
@@ -0,0 +1,58 @@
+package com.Piyush;
+
+import javax.sound.sampled.Line;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class C56_LinkedList {
+    public static void main(String[] args) {
+        LinkedList<Integer> l1 = new LinkedList<>();
+        l1.add(6);
+        l1.add(5);
+        l1.add(4);
+        l1.add(6);
+        l1.add(1);
+        l1.add(0,45);                      //isse hum usme index bhi de dete hai
+        l1.add(2,945);
+
+        LinkedList<Integer> l2=new LinkedList<>();
+        l2.add(150);
+        l2.add(110);
+        l2.add(120);
+
+
+//        l1.addAll(l2);                                            //addAll se hum ek Arraylist ko dusre me daal skte hai..and ye l1 ke end me append krega
+        l1.addAll(5,l2);                                            //5 index se shuru hojenge add hona
+        l1.add(1);
+
+//        l1.clear();
+
+
+//        for(int i=0;i<l1.size();i++){
+//            System.out.println(l1.get(i));
+//        }
+
+        System.out.println(l1.contains(150));
+        System.out.println(l1.indexOf(150));
+        System.out.println(l1.indexOf(1110));
+        System.out.println(l1.indexOf(1));
+        System.out.println(l1.lastIndexOf(1));
+
+        for(int i=0;i<l1.size();i++){
+            System.out.print(l1.get(i)+" ");
+        }
+        System.out.println();
+
+        l1.set(3,789456);
+
+
+        l1.addFirst(45614);
+        l1.addLast(450123);
+
+        for(int i=0;i<l1.size();i++){
+            System.out.print(l1.get(i)+" ");
+        }
+        System.out.println();
+        System.out.println(l1.subList(3,6));
+    }
+}
