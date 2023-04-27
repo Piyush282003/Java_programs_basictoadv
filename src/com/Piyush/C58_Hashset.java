43775151f1a0933f8962374945d0eec41c858b07
Piyush <palpiyush2003@gmail.com>
2023-04-27 13:03:07 +0530

    HashSet

---
+++ src/com/Piyush/C58_Hashset.java
@@ -0,0 +1,18 @@
+package com.Piyush;
+
+import java.util.HashSet;
+
+public class C58_Hashset {
+    public static void main(String[] args) {
+        HashSet<Integer> h1 = new HashSet<>();
+        HashSet<Integer> h2 = new HashSet<>(6,0.5f);
+        h1.add(4);
+        h1.add(1);
+        h1.add(5);
+        h1.add(78);
+        h1.add(756);
+
+//        without loop se bhi hum saare elements print kr skte hai..
+        System.out.println(h1);
+    }
+}
