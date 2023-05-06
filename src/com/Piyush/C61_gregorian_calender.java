d58a2cd5de7148bcd5a0ec8c391d8509c1eff587
Piyush <palpiyush2003@gmail.com>
2023-05-06 15:03:19 +0530

    Calendar

---
+++ src/com/Piyush/C61_gregorian_calender.java
@@ -0,0 +1,18 @@
+package com.Piyush;
+
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
+
+public class C61_gregorian_calender {
+    public static void main(String[] args) {
+        GregorianCalendar g = new GregorianCalendar();
+        System.out.println(g.isLeapYear(2020));
+
+
+
+//        below wale list denge timezones ki
+//        System.out.println(TimeZone.getAvailableIDs()[0]);
+//        System.out.println(TimeZone.getAvailableIDs()[1]);
+//        System.out.println(TimeZone.getAvailableIDs()[2]);
+    }
+}
