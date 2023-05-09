f687e6c422f550c74a99b85b1002367183183587
Piyush <palpiyush2003@gmail.com>
2023-05-09 14:40:56 +0530

    Duplicate Numbers in Array

--- src/ArraysPrac/J06_Duplicate_num.java
+++ src/ArraysPrac/J06_Duplicate_num.java
@@ -25,3 +25,35 @@ public class J06_Duplicate_num {
         }
     }
 }
+
+//// Map<Integer,Integer> mp=new HashMap<>();
+//    // for(int i=0;i<nums.length;i++){
+//    //     int c=nums[i];
+//    //     mp.put(c,mp.getOrDefault(c,0)+1);
+//    // }
+//    // int i=0,ans=0;;
+//    // for(Map.Entry<Integer,Integer> e:mp.entrySet()){
+//    //     if(i<e.getValue()){
+//    //         ans=e.getKey();
+//    //         i=e.getValue();
+//    //     }
+//    //     // System.out.println(ans);
+//    // }
+//    // return ans;
+//
+//    int n=arr.length;
+//        for(int i=0;i<n;i++){
+//        arr[arr[i]%n]+=n;
+////            arr[arr[i]%n]=arr[arr[i]%n]+n;
+//        }
+//
+////        System.out.println("Now the array is:-");
+////        for(Integer i:arr){
+////            System.out.println(i);
+////        }
+//        for(int i=0;i<n;i++){
+//        if(arr[i]>=n*2){
+//        return i;
+//        }
+//        }
+//        return -1;
